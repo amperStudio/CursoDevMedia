dfee4ceef61805f689525ad2a5be6e8aa58236c3
Jp martinez <jp@amperstudio.com.br>
2016-01-24 01:06:25 -0200

    classes paginacao

---
+++ Quiron.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        //quantos itens no banco
+        public int ItensTotal { get; set; }
+        //itens por página
+        public int ItensPorPagina { get; set; }
+        //página exibida no momento
+        public int PaginaAtual { get; set; }
+        //total de páginas
+        public int TotalDePaginas {
+            get { return (int) Math.Ceiling((decimal) ItensTotal/ItensPorPagina); }
+        }
+
+    }
+}
